01d3169494cd3dc17f77b11fe03027005528959f
Antoine C <antoine.cesbron.perso@gmail.com>
2023-05-14 22:52:56 +0200

    Cleaning

--- DB/Breweries.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO "public"."Breweries" ("id", "name", "address", "createdAt", "updatedAt") VALUES
-(4, 'Brasserie d''Achouffe', 'Achouffe n°32 - 6666 Wibrin - Achouffe', '2023-05-13 15:12:48.855+02', '2023-05-13 15:12:48.855+02');
-INSERT INTO "public"."Breweries" ("id", "name", "address", "createdAt", "updatedAt") VALUES
-(5, 'Brasserie Vanuxeem', 'Rue d’Armentières, 150, 7782 Ploegsteert', '2023-05-13 15:22:21.955+02', '2023-05-13 15:22:21.955+02');
-INSERT INTO "public"."Breweries" ("id", "name", "address", "createdAt", "updatedAt") VALUES
-(6, 'Brasserie St.Bernardus', 'Trappistenweg 23 8978 Watou', '2023-05-13 15:23:05.818+02', '2023-05-13 15:23:05.818+02');
-INSERT INTO "public"."Breweries" ("id", "name", "address", "createdAt", "updatedAt") VALUES
-(3, 'Brasserie Chimay', 'Rue de Poteaupré 5, B-6464 Bourlers', '2023-05-08 16:18:55.75+02', '2023-05-13 16:47:50.082+02');
